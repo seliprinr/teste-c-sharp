105cc12e8767f353d30eb850aa6e57c2a23e9d2d
Rodrigo Seliprin <rseliprin@gmail.com>
2023-04-20 06:24:33 -0300

    Switch em C#

--- Program.cs
+++ Program.cs
@@ -10,11 +10,21 @@ Console.WriteLine("Qual a sua idade?");
 
 idade = Convert.ToInt32(Console.ReadLine());
 
-if (idade == 34 && nomecompleto == "Rodrigo Seliprin")
+//if (idade == 34 && nomecompleto == "Rodrigo Seliprin")
+//{
+//    Console.WriteLine(textoolamundo + nomecpmpleto + "que tem" + idade + "anos");
+//}
+//else
+//{
+//    Console.WriteLine("Você errou a idade e/ou o nome");
+//}
+
+
+switch(nomecompleto)
 {
-    Console.WriteLine(textoolamundo + nomecpmpleto + "que tem" + idade + "anos");
-}
-else
-{
-    Console.WriteLine("Você errou a idade e/ou o nome");
+    case "Daniel Castello": Console.WriteLine("Professor de C#"); break;
+    case "Rafael": Console.WriteLine("Professor de Vue.Js"); break;
+    case "Filipe": Console.WriteLine("Professor de Python"); break;
+    case "Kaique": Console.WriteLine("Professor de Kotlin"); break;
+    default: Console.WriteLine("Não possuímos esse professor"); break;
 }
