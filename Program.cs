e1a7fa08a684b0f69369ac3a03c4e9df00318d9e
Rodrigo Seliprin <rseliprin@gmail.com>
2023-04-20 06:16:25 -0300

    Testes de código em C#

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello my friend");
+﻿const string textoolamundo = "Hey!"
+string nomecompleto;
+int idade;
+
+Console.WriteLine("Qual o seu nome completo?");
+
+nomecompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual a sua idade?");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade == 34 && nomecompleto == "Rodrigo Seliprin")
+{
+    Console.WriteLine(textoolamundo + nomecpmpleto + "que tem" + idade + "anos");
+}
+else
+{
+    Console.WriteLine("Você errou a idade e/ou o nome");
+}
